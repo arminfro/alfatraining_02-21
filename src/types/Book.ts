gmiDash/alfatraining_12-20

A2, add BookList, BookListItem

src/types/Book.ts
@@ -1,0 +1,15 @@
+export default interface Book {
+  isbn: string;
+  title: string;
+  authors: string[];
+  published: Date;
+  subtitle?: string;
+  rating?: number;
+  thumbnails: IThumbnail[];
+  description?: string;
+}
+
+export interface IThumbnail {
+  url: string;
+  title?: string;
+}

arminfro/alfatraining_02-21

src/types/Book.ts
@@ -1,0 +1,15 @@
+export default interface Book {
+  isbn: string;
+  title: string;
+  authors: string[];
+  published: Date;
+  subtitle?: string;
+  rating?: number;
+  thumbnails: Thumbnail[];
+  description?: string;
+}
+
+export interface Thumbnail {
+  url: string;
+  title?: string;
+}